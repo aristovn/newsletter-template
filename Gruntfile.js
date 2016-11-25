c662ebd6279572f6fc4b59fcc43c7c6dd170aaf9
Nikita Aristov <nikita.aristov@gmail.com>
2016-11-25 15:14:17 +0300

    Mixins update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,34 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-    // Project configuration.
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        concat: {
-            jade: {
-                src: 'blocks/**/*.jade',
-                dest: 'temp/concat.jade'
-            }
-        },
-        jade: {
-            compile: {
-                files: {
-                    'build/index.html': 'pages/index.jade'
-                }
-            }
-        },
-        watch: {
-            templates: {
-                files: ['pages/**/*.jade', 'blocks/**/*.jade'],
-                tasks: ['concat', 'jade'],
-                options: {
-                    livereload: true
-                }
-            }
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      jade: {
+        src: 'blocks/**/*.jade',
+        dest: 'temp/concat.jade'
+      }
+    },
+    jade: {
+      compile: {
+        files: {
+          'build/index.html': 'pages/index.jade'
         }
-    });
+      }
+    },
+    watch: {
+      templates: {
+        files: ['pages/**/*.jade', 'blocks/**/*.jade'],
+        tasks: ['concat', 'jade'],
+        options: {
+          livereload: true
+        }
+      }
+    }
+  })
 
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-jade');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.registerTask('default', ['concat', 'jade']);
-};
+  grunt.loadNpmTasks('grunt-contrib-concat')
+  grunt.loadNpmTasks('grunt-contrib-jade')
+  grunt.loadNpmTasks('grunt-contrib-watch')
+  grunt.registerTask('default', ['concat', 'jade'])
+}
